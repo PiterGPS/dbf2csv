c5468b6aca3e2e3dd331034f6a97b43db18e4f71
Piter Gavrinev <piter554@rambler.ru>
2014-11-27 11:01:06 +0300

    program files

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace dbf2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            if (args.Length < 1)
+            {
+                Console.WriteLine(@" This is DBase IV to csv (tab delimiters) simple converter");
+                Console.WriteLine(@" generate csv files with ANSI (1251) text");
+                Console.WriteLine(@"");
+                Console.WriteLine(@" HOW USE IT:");
+                Console.WriteLine(@">dbf2csv disk:\inpdir\*.dbf [disk:\outdir]");
+                Console.WriteLine(@"Piter Gavrinev 2010 :)");
+                Console.WriteLine(@"need .NET 2.0 vesrion");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine(@"Cool dbf to csv Piter converter.");
+            string inp = args[0];
+            string cr = "";
+            int mp = inp.LastIndexOf("\\");
+            if (mp == -1)
+            {
+                Console.WriteLine(@"error, need full path to files: disk:path/path/*.dbf");
+                return;
+            }
+
+                cr = inp.Substring(mp + 1);
+                inp = inp.Substring(0, mp);
+
+                string out_directory = inp;
+                if (args.Length >= 2)
+                {
+                    out_directory = args[1];
+                }
+
+            string[] files = System.IO.Directory.GetFiles(inp,cr);
+
+            if (!System.IO.Directory.Exists(out_directory))
+            {
+                System.IO.Directory.CreateDirectory(out_directory);
+            }
+            Console.WriteLine(@"OutDirectory: " + out_directory);
+            dbfToCSV dtc = new dbfToCSV();
+            for (int i = 0; i < files.Length; i++)
+            {
+                string flnm = Path.GetFileNameWithoutExtension(files[i]);
+                string out_file = out_directory + "\\" + flnm + ".csv";
+                Console.Write("conv: " + files[i]);
+                
+                dtc.csv_filename = out_file;
+                dtc.dbf_filename = files[i];
+                if (dtc.Do())  Console.Write(" OK.\r\n");
+            }
+            
+            Console.WriteLine(@"End.");
+            
+        }
+    }
+}
---
+++ dbfToCSV.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+namespace dbf2csv
+{
+    
+    class dbfToCSV
+    {
+        public string dbf_filename;
+        public string csv_filename;
+        
+        public  dbfToCSV()
+        {
+           
+        }
+
+        
+
+        public bool Do()
+        {
+            try{
+            string Line = "";
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Path.GetDirectoryName(dbf_filename) + ";Extended Properties=dBASE IV;";
+            System.IO.StreamWriter sw = new StreamWriter(csv_filename, false, Encoding.GetEncoding("windows-1251"));
+            OleDbConnection oc = new OleDbConnection(strConn);
+            oc.Open();
+            
+            OleDbCommand command = new OleDbCommand("select * from " + Path.GetFileName(dbf_filename), oc);
+
+            bool write_header = true;
+
+            // Execute the DataReader and access the data.
+            OleDbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                if (write_header)
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        if (Line != "") Line += "\t";
+                        Line += reader.GetName(i);
+                    }
+                    write_header = false;
+                    sw.WriteLine(Line);
+                }
+
+                Line = "";
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                   
+                    string ttn=reader.GetDataTypeName(i);
+                    string vp="";
+                    if (!reader.IsDBNull(i))
+                    {
+                        if (ttn == "DBTYPE_DATE" || ttn=="DBTYPE_DBTIMESTAMP")
+                        {
+
+                            vp = reader.GetDateTime(i).ToString("dd.MM.yyyy");
+
+
+                        }
+
+                        if (ttn == "DBTYPE_14" )
+                        {
+                            vp = reader.GetInt32(i).ToString();
+                            vp = vp.Replace(",", ".");
+
+                        }
+
+                        if (ttn == "DBTYPE_BOOL")
+                        {
+                            bool bl = reader.GetBoolean(i);
+                            if (bl)
+                            {
+                                vp = "true";
+                            }else{
+                                vp = "false";
+                            }
+
+                        }
+
+                        if (ttn == "DBTYPE_DECIMAL")
+                        {
+                            vp = reader.GetDecimal(i).ToString();
+                            vp = vp.Replace(",", ".");
+
+                        }
+
+                        if (ttn == "DBTYPE_R8" || ttn == "DBTYPE_CY")
+                        {
+                            vp = reader.GetDouble(i).ToString("#.##");
+                            vp = vp.Replace(",", ".");
+                          
+                        }
+
+                        if (ttn == "DBTYPE_STR" || ttn == "DBTYPE_WSTR" || ttn == "DBTYPE_WVARCHAR")
+                        {
+                            vp = reader.GetString(i);
+                        }
+                    }
+                     if (Line != "") Line += "\t";
+                     Line += vp;
+                }
+                sw.WriteLine(Line);
+            }
+
+            // Call Close when done reading.
+            sw.Close();
+            reader.Close();
+            return true;
+         }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: "+ex.Message);
+                return false;
+          }
+            
+
+        }
+      
+
+    }
+}
